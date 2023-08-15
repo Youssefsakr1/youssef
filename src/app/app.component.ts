c3a532fd66804beda28addc872350ced75d74f9b
Youssefsakr1 <youssefsakr119@gmail.com>
2023-08-15 13:32:28 +0200

    ss

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'test';
+  name='youssef';
+  grade='three';
+  img="https://picsum.photos/200/300"
+  
+
+
+
+  
+}
